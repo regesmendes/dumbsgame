efedd3320edb705142682eeacbf5964cfdf4610d
Reges Mendes <reges.mendes@gmail.com>
2022-07-24 15:26:53 +0100

    converted to vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Dumb's Game</h1>
+</template>
+
+<script>
+export default {
+    name: 'HomeView',
+
+    data: function() {
+        return {
+        }
+    },
+
+    mounted() {
+        console.log('component mounted')
+    }
+}
+</script>
